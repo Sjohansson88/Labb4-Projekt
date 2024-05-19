f6800719dd04ab10ef5b89b31bbdecebd772e202
Sjohansson88 <145502017+Sjohansson88@users.noreply.github.com>
2024-05-19 21:56:27 +0200

    Add files via upload

---
+++ SUT23-Labb4Models/BookingHistory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUT23_Labb4Models
+{
+    public class BookingHistory
+    {
+        [Key]
+        public int Id { get; set; } 
+        public int BookingId { get; set; }
+        public DateTime? OldStartTime { get; set; } 
+        public DateTime? OldEndTime { get; set; } 
+        public DateTime NewStartTime { get; set; }
+        public DateTime NewEndTime { get; set; }
+        public DateTime ChangedAt { get; set; }
+        public string ChangedBy { get; set; }
+    }
+}
---
+++ SUT23-Labb4Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SUT23_Labb4Models
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public ICollection<Appointment> Appointments { get; set; }
+       
+    }
+}
